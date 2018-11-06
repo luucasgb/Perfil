9e719449c1354ef25330206e9734cb6f2754b5c4
luucasgb <44594820+luucasgb@users.noreply.github.com>
2018-11-06 16:10:52 -0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,17 +1,25 @@
 #include <stdio.h>
 #include <string.h>
 
-void minuscula(char s[]) {
-   int c = 0;
-   while (s[c] != '\0') {
-        if (s[c] >= 'A' && s[c] <= 'Z') {
-            s[c] = s[c] + 32;
-        }
-        c++;
-    }
-}
+void minuscula(char s[]);
 
-int main(void){
+typedef struct _CARTA {
+    int numero;
+    char tipo[20];
+    char resposta[20];
+    char dica1[128];
+    char dica2[128];
+    char dica3[128];
+    char dica4[128];
+    char dica5[128];
+    char dica6[128];
+    char dica7[128];
+    char dica8[128];
+    char dica9[128];
+    char dica10[128];
+} CARTA;
+
+int main(void) {
     char a[64] = "freddie", chute[64];
     puts("Chute um nome de cantor: ");
     scanf("%s",chute);
@@ -23,4 +31,14 @@ int main(void){
         printf("Erroooou!");
     }
     return 1;
+}
+
+void minuscula(char s[]) {
+   int c = 0;
+   while (s[c] != '\0') {
+        if (s[c] >= 'A' && s[c] <= 'Z') {
+            s[c] = s[c] + 32;
+        }
+        c++;
+    }
 }
